aef0e5728ca5076624cb9be3788e040143379077
Ankur Seth <iankurseth@gmail.com>
2023-05-08 21:36:56 +0530

    Added 1 true 1 false tests for each func in array and object modules

--- tests/array.test.ts
+++ tests/array.test.ts
@@ -2,9 +2,44 @@ const whichtype = require("../dist");
 
 
 
-test("isHttp", () => {
-	const { isHttp } = whichtype;
+test("isArrayOfArrays", () => {
+	const { isArrayOfArrays } = whichtype;
 
-	expect(isHttp("http://foo.com")).toBe(true);
-	expect(isHttp("https://foo.com")).toBe(false);
+	expect(isArrayOfArrays([[], []])).toBe(true);
+	expect(isArrayOfArrays("foo")).toBe(false);
+});
+
+test("isArrayOfBooleans", () => {
+	const { isArrayOfBooleans } = whichtype;
+
+	expect(isArrayOfBooleans([[], []])).toBe(true);
+	expect(isArrayOfBooleans("foo")).toBe(false);
+});
+
+test("isArrayOfChars", () => {
+	const { isArrayOfChars } = whichtype;
+
+	expect(isArrayOfChars([[], []])).toBe(true);
+	expect(isArrayOfChars("foo")).toBe(false);
+});
+
+test("isArrayOfNumbers", () => {
+	const { isArrayOfNumbers } = whichtype;
+
+	expect(isArrayOfNumbers([[], []])).toBe(true);
+	expect(isArrayOfNumbers("foo")).toBe(false);
+});
+
+test("isArrayOfObjects", () => {
+	const { isArrayOfObjects } = whichtype;
+
+	expect(isArrayOfObjects([[], []])).toBe(true);
+	expect(isArrayOfObjects("foo")).toBe(false);
+});
+
+test("isArrayOfStrings", () => {
+	const { isArrayOfStrings } = whichtype;
+
+	expect(isArrayOfStrings([[], []])).toBe(true);
+	expect(isArrayOfStrings("foo")).toBe(false);
 });
--- tests/object.test.ts
+++ tests/object.test.ts
@@ -2,9 +2,44 @@ const whichtype = require("../dist");
 
 
 
-test("isHttp", () => {
-	const { isHttp } = whichtype;
+test("isObjectOfArrays", () => {
+	const { isObjectOfArrays } = whichtype;
 
-	expect(isHttp("http://foo.com")).toBe(true);
-	expect(isHttp("https://foo.com")).toBe(false);
+	expect(isObjectOfArrays({})).toBe(true);
+	expect(isObjectOfArrays("foo")).toBe(false);
+});
+
+test("isObjectOfBooleans", () => {
+	const { isObjectOfBooleans } = whichtype;
+
+	expect(isObjectOfBooleans({})).toBe(true);
+	expect(isObjectOfBooleans("foo")).toBe(false);
+});
+
+test("isObjectOfChars", () => {
+	const { isObjectOfChars } = whichtype;
+
+	expect(isObjectOfChars({})).toBe(true);
+	expect(isObjectOfChars("foo")).toBe(false);
+});
+
+test("isObjectOfNumbers", () => {
+	const { isObjectOfNumbers } = whichtype;
+
+	expect(isObjectOfNumbers({})).toBe(true);
+	expect(isObjectOfNumbers("foo")).toBe(false);
+});
+
+test("isObjectOfObjects", () => {
+	const { isObjectOfObjects } = whichtype;
+
+	expect(isObjectOfObjects({})).toBe(true);
+	expect(isObjectOfObjects("foo")).toBe(false);
+});
+
+test("isObjectOfStrings", () => {
+	const { isObjectOfStrings } = whichtype;
+
+	expect(isObjectOfStrings({})).toBe(true);
+	expect(isObjectOfStrings("foo")).toBe(false);
 });
